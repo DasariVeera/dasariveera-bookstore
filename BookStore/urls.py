d2e031d8f3458dc5a306c529c0097af10586752e
DasariVeera <68891410+DasariVeera@users.noreply.github.com>
2020-10-26 23:57:28 +0530

    Update urls.py

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('no-body-can-access-admin/', admin.site.urls),
     path('', include('books.urls')),
     path('payments/', include('payments.urls')),
+    path('users', include('django.contrib.auth.urls')),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
 ]
