7a21431cb56b96e28362083106deb5cf221fa21d
DasariVeera <68891410+DasariVeera@users.noreply.github.com>
2020-10-27 11:10:34 +0530

    Update settings.py

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -146,5 +146,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "veeratechsmart@gmail.com" 
-EMAIL_HOST_PASSWORD = "smartveera"
+EMAIL_HOST_USER = "smartprogrammer225@gmail.com" 
+EMAIL_HOST_PASSWORD = "smartprogram"
